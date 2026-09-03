ebfa123b2ad105931e185813e39196abf630d59e
xuluochangan <xuluochangan@users.noreply.github.com>
2026-09-04 00:33:01 +0800

    feat: enable Twikoo comments

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -3,10 +3,10 @@ import { SITE_LANG } from "./siteConfig";
 
 // 评论系统配置
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	enable: true, // 启用评论功能。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
-		envId: "https://twikoo.vercel.app",
+		envId: "https://my-chi-ashy.vercel.app",
 		lang: SITE_LANG,
 	},
 	giscus: {
